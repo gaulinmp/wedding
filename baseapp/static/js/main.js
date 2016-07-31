805f47792e3affe9e5e0643ffdc3cf23c1ad55c7
Mac <gaulinmp+git@gmail.com>
2016-07-31 13:47:34 -0600

    Added JS for tooltips and href targets

--- baseapp/static/js/main.js
+++ baseapp/static/js/main.js
@@ -1,3 +1,6 @@
-(function() {
-
-}).call(this);
+$(function () {
+  $("a[href^='http']").attr('target','_blank');
+  $("a[href^='//']").attr('target','_blank');
+  $('a[rel="external"]').attr('target', '_blank');
+  $("[rel='tooltip']").tooltip();
+});
