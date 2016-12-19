30fca10718fb6d8c49e10f0e7c257f36478fc8bd
Mac Gaulin <gaulinmp+git@gmail.com>
2016-12-18 23:31:57 -0600

    Added recaptcha string

--- config/default_server.py
+++ config/default_server.py
@@ -9,7 +9,6 @@ class Config(object):
     SITE_AUTHOR = "Mac Gaulin"
     SITE_DESCRIPTION = "Wedding site for Lacey and Mac."
     SECRET_KEY = os_env.get('SECRET_KEY', 'wegongetmarried')
-    # GOOGLE_ANALYTICS = 'UA-80889496-1'
     APP_DIR = os.path.abspath(os.path.dirname(__file__))  # config directory
     PROJECT_ROOT = os.path.abspath(os.path.join(APP_DIR, os.pardir))
     BCRYPT_LOG_ROUNDS = 13
@@ -23,6 +22,7 @@ class ProdConfig(Config):
     DEBUG = False
     DB_NAME = 'database.db'
     GOOGLE_ANALYTICS = 'UA-80889496-1'
+    GOOGLE_RECAPTCHA = '6LfpIA8UAAAAANnfnhrUF84a2z7ui-tlZN2msV17'
     DBFILE_PATH = os.path.join(Config.APP_DIR, DB_NAME)
     SQLALCHEMY_DATABASE_URI = ('sqlite:///' + DBFILE_PATH)
 
@@ -37,6 +37,7 @@ class DevConfig(Config):
     DB_NAME = 'dev.db'
     DBFILE_PATH = os.path.join(Config.APP_DIR, DB_NAME)
     SQLALCHEMY_DATABASE_URI = ('sqlite:///' + DBFILE_PATH)
+    GOOGLE_RECAPTCHA = '6LfpIA8UAAAAANnfnhrUF84a2z7ui-tlZN2msV17'
 
 
 class TestConfig(Config):
